ef52a90c1ac908d0b87dd61f98d79cb91af2edf0
Colin Rundel <rundel@gmail.com>
2020-10-17 21:42:16 +0100

    Actually fix things

--- R/encode.R
+++ R/encode.R
@@ -20,18 +20,18 @@ encode_obj = function(obj, compress = c("bzip2", "gzip", "xz", "none"))  {
 #'
 #' @export
 decode_obj = function(txt, compress = c("bzip2", "gzip", "xz", "none")) {
+  compress = match.arg(compress)
+
   if (txt == "")
     return(list())
 
   res = try({
-    compress = match.arg(compress)
-
     comp_raw = base64enc::base64decode(txt)
     raw = memDecompress(comp_raw, type = compress)
     unserialize(raw)
   }, silent = TRUE)
 
-  if (inherits("try-error"))
+  if (inherits(res, "try-error"))
     res = list()
 
   res
