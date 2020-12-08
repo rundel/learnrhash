714511c8b0de7cb816e99fe01551e2fef9255d5f
Colin Rundel <rundel@gmail.com>
2020-12-08 13:50:09 +0000

    Add clipboard stuff

--- NEWS.md
+++ NEWS.md
@@ -5,3 +5,5 @@
 * Added a `NEWS.md` file to track changes to the package.
 
 * Added `strip_output` argument to `encoder_logic` to help keep hash sizes down.
+
+* Added javascript support for selecting and copying hash to clipboard.
--- R/shiny.R
+++ R/shiny.R
@@ -157,25 +157,124 @@ encoder_ui = function(ui_before = default_ui(), ui_after = NULL) {
   check_not_server_context(parent.frame())
 
   shiny::tags$div(
+    class = "encoder_ui",
     ui_before,
     shiny::fixedRow(
       shiny::column(
         width = 3,
-        shiny::actionButton("hash_generate", "Generate Submission")
+        shiny::actionButton("hash_generate", "Generate", title = "Generate hash")
       ),
       shiny::column(width = 7),
       shiny::column(
-        width = 2
-        # TODO - Turn off for now since shinyapps.io doesn't like clipr
-        #shiny::actionButton("hash_copy", "Copy")
-      )
+        width = 2,
+        shiny::tags$div(
+          class = "btn-group btn-group-sm pull-right",
+          role = "group",
+          `aria-label` = "Clipboard buttons",
+
+          shiny::tags$button(
+            id="hash_select", class="btn btn-default", type="button",
+            title = "Select hash",
+            cursor_svg("16px")
+          ),
+
+          shiny::tags$span(class="btn-separator"),
+
+          shiny::tags$button(
+            id="hash_copy", class="btn btn-default", type="button",
+            title = "Copy hash to clipboard",
+            clipboard_svg("16px")
+          )
+        )
+      ),
+      style = "padding-bottom: 0.5em;"
     ),
-    shiny::tags$br(),
+    #shiny::tags$br(),
     wrapped_verbatim_text_output("hash_output", TRUE),
     shiny::tags$br(),
-    ui_after
+    ui_after,
+    encoder_clipboard_js()
   )
 }
+
+cursor_svg = function(height) {
+  shiny::HTML( paste0(
+    '<svg
+      aria-hidden="true" focusable="false"
+      data-prefix="fas" data-icon="i-cursor"
+      class="svg-inline--fa fa-i-cursor fa-w-8"
+      role="img" xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 512"',
+    ' style="height: ', height, ';"',
+    '>
+      <path fill="currentColor" d="M256 52.048V12.065C256 5.496 250.726.148 244.158.066 211.621-.344 166.469.011 128 37.959 90.266.736 46.979-.114 11.913.114 5.318.157 0 5.519 0 12.114v39.645c0 6.687 5.458 12.078 12.145 11.998C38.111 63.447 96 67.243 96 112.182V224H60c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h36v112c0 44.932-56.075 48.031-83.95 47.959C5.404 447.942 0 453.306 0 459.952v39.983c0 6.569 5.274 11.917 11.842 11.999 32.537.409 77.689.054 116.158-37.894 37.734 37.223 81.021 38.073 116.087 37.845 6.595-.043 11.913-5.405 11.913-12V460.24c0-6.687-5.458-12.078-12.145-11.998C217.889 448.553 160 444.939 160 400V288h36c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12h-36V112.182c0-44.932 56.075-48.213 83.95-48.142 6.646.018 12.05-5.346 12.05-11.992z">
+      </path>
+    </svg>'
+  ) )
+}
+
+clipboard_svg = function(height) {
+  shiny::HTML( paste0(
+    '<svg
+      aria-hidden="true" focusable="false"
+      data-prefix="far" data-icon="clipboard"
+      class="svg-inline--fa fa-clipboard fa-w-12"
+      role="img" xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"',
+    ' style="height: ', height, ';"',
+    '>
+      <path fill="currentColor" d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z">
+      </path>
+    </svg>'
+  ) )
+}
+
+
+encoder_clipboard_js = function() {
+  list(
+    shiny::tags$script( shiny::HTML(
+      "selectText = function(node) {
+        node = document.getElementById(node);
+
+        if (document.body.createTextRange) {
+          const range = document.body.createTextRange();
+          range.moveToElementText(node);
+          range.select();
+        } else if (window.getSelection) {
+          const selection = window.getSelection();
+          const range = document.createRange();
+          range.selectNodeContents(node);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        } else {
+          console.warn('Could not select text in node: Unsupported browser.');
+        }
+      }"
+    ) ),
+
+    shiny::tags$script( shiny::HTML(
+      "document.getElementById('hash_select').addEventListener('click', function(e) {
+        e.preventDefault();
+
+        selectText('hash_output');
+      });"
+    ) ),
+
+    shiny::tags$script( shiny::HTML(
+      "document.getElementById('hash_copy').addEventListener('click', function(e) {
+        e.preventDefault();
+
+        selectText('hash_output');
+        document.execCommand('copy');
+      });"
+    ) )
+  )
+}
+
+
+
+
+
 
 wrapped_verbatim_text_output = function(outputId, placeholder = FALSE) {
   x = shiny::verbatimTextOutput(outputId, placeholder)
