e09e1c0f460c93f18b80b857d0462d0647039bae
Colin Rundel <rundel@gmail.com>
2021-09-08 22:37:14 -0400

    Extraction code was not updated for the new hashed object structure so everything was broken - should be fixed now.
    Consider simplify / deprecating functions in the future.
    
    TODO - does answer need to be a list column?
    
    Fixes #16

--- R/extract.R
+++ R/extract.R
@@ -6,16 +6,14 @@
 #' @description
 #' The following are helper functions for extracting data from hashed learnr solutions.
 #'
-#' * `extract_hash` - extracts the contents of the hashes into a list column.
+#' * `extract_hash` - extracts the contents of the hashes into label, type, answer, correct, and timestamp columns
 #'
-#' * `extract_questions` - extracts the contents of the hashes and expands answered questions.
+#' * `extract_questions` - extracts the contents of the hashes for answered questions.
 #'
-#' * `extract_exercises` - extracts the contents of the hashes and expands answered exercises.
+#' * `extract_exercises` - extracts the contents of the hashes for answered exercises.
 #'
 #' @param df A data frame containing the hashes
 #' @param hash The name of the column containing the hashes
-#' @param include_output Logical. Should the exercises' code output be included.
-#' @param include_text Logical. Should the questions' text be included.
 #'
 NULL
 
@@ -39,94 +37,21 @@ extract_hash = function(df, hash) {
   df %>%
     dplyr::rename(hash = {{hash}}) %>%
     dplyr::mutate(
-      hash = lapply(hash, learnrhash::decode_obj),
-      hash = lapply(hash, fix_empty_state_obj)
+      hash = lapply(hash, learnrhash::decode_obj)
     ) %>%
-    tidyr::unnest_longer(hash) %>%
-    tidyr::unnest_wider(hash,)
+    tidyr::unnest(hash)
 }
 
 #' @rdname extract
 #' @export
-extract_exercises = function(df, hash, include_output = FALSE) {
-  d = df %>%
-    extract_hash(hash) %>%
-    dplyr::filter(.data$type == "exercise_submission")
-
-
-  if (nrow(d) == 0) {
-    # Since we don't know the other column names we need to do this
-    d = d %>%
-      dplyr::select(-dplyr::any_of(c("type", "data", "id"))) %>%
-      dplyr::mutate(
-        exercise_id = character(),
-        code = character(),
-        output = list(),
-        feedback = list(),
-        checked = logical(),
-        correct = logical()
-      )
-  } else {
-    d = d %>%
-      tidyr::hoist( # list columns
-        "data", "output", "feedback", .simplify = FALSE
-      ) %>%
-      tidyr::hoist( # vector columns
-        "data", "code", "checked"
-      ) %>%
-      dplyr::select(-.data$type) %>%
-      dplyr::rename(exercise_id = .data$id) %>%
-      dplyr::relocate(
-        .data$exercise_id, .data$code, .data$output,
-        .data$feedback, .data$checked,
-        .after = dplyr::last_col()
-      ) %>%
-      dplyr::mutate(
-        correct = purrr::map_lgl(.data$feedback, "correct", .default = NA)
-      )
-  }
-
-  if (!include_output)
-    d = dplyr::select(d, -.data$output)
-
-  d
+extract_exercises = function(df, hash) {
+  extract_hash(df, hash) %>%
+    dplyr::filter(type == "exercise")
 }
 
 #' @rdname extract
 #' @export
-extract_questions = function(df, hash, include_text = TRUE) {
-  # TODO - Fix me if learnr PR accepted
-
-  d = extract_hash(df, hash) %>%
-    dplyr::filter(.data$type == "question_submission")
-
-  if (nrow(d) == 0) {
-    # Since we don't know the other column names we need to do this
-    d = d %>%
-      dplyr::select(-dplyr::any_of(c("type", "data", "id"))) %>%
-      dplyr::mutate(
-        question_id = character(),
-        question_text = character(),
-        answer = list()
-        #correct = logical()
-      )
-  } else {
-    d = d %>%
-      tidyr::unnest_wider(.data$data) %>%
-      dplyr::select(-.data$type, -.data$api_version) %>%
-      dplyr::rename(
-        question_id = .data$id,
-        question_text = .data$question
-      ) %>%
-      dplyr::relocate(
-        #question_id, question_text, answer, correct, .after = last_col()
-        .data$question_id, .data$question_text, .data$answer,
-        .after = dplyr::last_col()
-      )
-  }
-
-  if (!include_text)
-    d = dplyr::select(d, -.data$question_text)
-
-  d
+extract_questions = function(df, hash) {
+  extract_hash(df, hash) %>%
+    dplyr::filter(type == "question")
 }
